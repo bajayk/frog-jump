4bca787b0ad6927dd69f40f9cc3c336499e944bc
Ajay Badgujar <bajayk@gmail.com>
2019-10-15 19:23:12 +0530

    Part 3

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 var queue
 var stage, canvas;
 var riverStreamMatrix, riverStream;
+var insects_sequence;
+var frog_sequence;
+var frog;
+
+
+var insects = [];
+var leaves = [];
 
 //constants
 const SIDE_GRASS_WIDTH = 200;
 const RIVER_WIDTH = 400;
 const FOLIAGE_WIDTH = 66;
+
+var frog_data = {
+    "images":["assets/frog_spritesheet.png"],
+    "frames":[
+        [0,0,70,90],
+        [70, 0, 70, 90]
+    ],
+    "animations":{
+        "seating":[0],
+        "jumping":[1]
+    }
+}
+
+var insects_data = {
+    "images":["assets/insects_spritesheet.png"],
+    "frames":[
+        [0,0,50,50],
+        [50,0,50,50],
+        [100,0,50,50],
+        [0,50,50,50],
+        [50,50,50,50],
+        [100,50,50,50]
+    ],
+    "animations":{
+        "insect1":[0],
+        "insect2":[1],
+        "insect3":[2],
+        "insect4":[3],
+        "insect5":[4],
+        "insect6":[5]
+    }
+}
+
 
 
 function preload(){
@@ -25,6 +65,17 @@ function init(){
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas);
     buildGameBackground();
+    createInsects();
+    addLeavesAndInsects();
+    addFrog();    
+    startGame();
+}
+
+function startGame(){
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.addEventListener('tick', function(e){
+        stage.update();
+    });
 }
 
 function buildGameBackground(){    
@@ -60,6 +111,60 @@ function buildGameBackground(){
     foliageRight.shadow = new createjs.Shadow("rgba(0,0,0,0.8)",0,0,10);
     stage.addChild(foliageRight);
 
-    stage.update();
+    
 
+}
+
+function createInsects(){
+    insects_sequence = new createjs.SpriteSheet(insects_data);
+    for(var i=0; i<6; i++){
+        var insect = new createjs.Sprite(insects_sequence);
+        insect.regX = 25;
+        insect.regY = 25;
+        insect.gotoAndStop(i);
+        insects.push(insect);
+    }
+}
+
+function addLeavesAndInsects(){
+    var leafTexture = queue.getResult('leaf');
+
+    for(var i=0; i<5; i++){
+
+        var leafContainer = new createjs.Container();
+        leafContainer.x = SIDE_GRASS_WIDTH + 100 + Math.floor(Math.random() * 200);
+        leafContainer.y = 150 * i + 50;
+        leafContainer.velX = Math.random()*2 - 1;
+        leafContainer.velY = 0.75;
+        leafContainer.rotationStep = Math.random()*1 - 0.5;
+
+        //Add leaf to container
+        var leaf = new createjs.Shape();
+        var leafMetrix = new createjs.Matrix2D();
+        leafMetrix.translate(-50,-50);
+        leaf.graphics.beginBitmapFill(leafTexture,'no-repeat',leafMetrix);
+        leaf.graphics.drawCircle(0,0,50);        
+        leafContainer.addChild(leaf);
+
+        addInsect(leafContainer);
+
+        stage.addChild(leafContainer);
+        leaves.push(leafContainer);
+    }
+
+}
+
+function addInsect(leafContainer){
+    var insect = insects[Math.floor(Math.random()*5)].clone();        
+    leafContainer.addChild(insect);    
+}
+
+function addFrog(){
+    frog_sequence = new createjs.SpriteSheet(frog_data);
+    frog = new createjs.Sprite(frog_sequence);
+    frog.gotoAndStop('seating');
+    frog.regX = 35;
+    frog.regY = 45;
+    frog.status = "seating";
+    stage.addChild(frog);
 }
